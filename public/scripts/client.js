98fb8975503bc4ad82da4b5ea32d13193fa1ed7c
Edward <edward.ascan@me.com>
2021-01-28 03:41:22 +0000

    commiting everything so far, forgot completely about committing to github

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,115 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+
+$(document).ready(function() {
+
+// render each tweet form database
+
+const createTweetElement = function (tweetObj) {
+  const tweetEL = `
+  
+  <article class="tweetArticle">
+
+      <header class=allTweets>
+        <div class="posterName">
+          <img src="${tweetObj.user.avatars}" class="male">
+          <p class=user>${tweetObj.user.name}</p>
+        </div>
+        <div class="username">
+          <p>${tweetObj.user.handle}</p>
+        </div>
+      </header>
+      <div class="tweetContent">
+        <p class="userTweet">${tweetObj.content.text}</p>
+        <p class="empty"></p>
+      </div>
+      <footer>
+        <div class="datePosted">
+        ${tweetObj.created_at}
+        </div>
+        <div>
+          <img src="/images/shareIcons.png" class="share">
+        </div>
+      </footer>
+
+    </article>
+    `;
+
+    return tweetEL;
+
+}
+
+const renderTweets = function(tweets) {
+  // loops through tweets
+
+  for (const tweet of tweets) {
+    // calls createTweetElement for each tweet
+    const newTweet = createTweetElement(tweet);
+
+    // takes return value and appends it to the tweets container
+    $("#tweets").append(newTweet);
+  }
+}
+
+renderTweets(data);
+// const $tweet = createTweetElement(userData);
+
+// // Test / driver code (temporary)
+// console.log($tweet); // to see what it looks like
+
+
+    // --- our code goes here ---
+
+    $("#writeTweet").on("submit", function (event) {
+      // preventing browser from default submitting
+      event.preventDefault();
+
+      
+      // read data from input text, taret the textarea
+      const textArea = $(this).find("#tweet-text");
+
+      const userInput = textArea.val();
+
+      // console.log(userInput.length);
+      //will need to empty the tweets section later
+
+
+      // using DOM traversal to go up to ancestor form and down to counter
+
+      
+      // console.log(counter);
+      
+      
+      
+      //empty user input area after submission:
+      textArea.val("");
+
+    })
+})
---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,33 @@
+//working with the new tweet submissions
+
+$(document).ready(function() {
+
+  // function to change the counter value
+
+  $("#tweet-text").on("keyup", function (event) {
+    const userInput = $(this).val().length;
+    
+    const counter = $(this).closest("#writeTweet").find(".counter");
+
+    const decreasingVal = 140 - userInput;
+
+    const maxChar = 140;
+    
+  
+// if input over limit, counter turns red
+    if (userInput <= maxChar) {
+      counter.removeClass("red");
+      counter.text(decreasingVal)
+
+    } else if (userInput > maxChar) {
+      counter.addClass("red").text(decreasingVal);
+    } 
+    // return counter.text() - counter.val();
+    // console.log(counter.text())
+
+  })
+
+
+
+
+});
